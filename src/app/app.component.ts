caa131f350f39251ce3992ebd697b78f6e20cc24
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-06-20 01:11:20 -0300

    Adicionando o observable para o componente de erro.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { NotificationService } from './core/services/notification.service';
+import { ModalComponent } from './shared/message/modal/modal.component';
 
 @Component({
   selector: 'tcc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tcc';
+export class AppComponent implements OnInit {
+  constructor(
+    private dialog: MatDialog,
+    private notification: NotificationService
+  ) {}
+
+  ngOnInit() {
+    this.notification.notifier.subscribe(message =>
+      this.dialog.open(ModalComponent, {
+        minWidth: '300px',
+        data: { message: message, title: 'ERRO', error: true }
+      })
+    );
+  }
 }
