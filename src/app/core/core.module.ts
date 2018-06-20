01d5c609048f23f49b43054e3e699aa9510840d4
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-06-20 01:23:26 -0300

    Import dos Services e do DataSouce criados.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,7 +7,10 @@ import {AuthGuard} from './guard/auth.guard';
 import {NotificationService} from './services/notification.service';
 import {GrupoService} from './services/administracao/grupos.service';
 import {GruposDataSouce} from './services/administracao/grupos.datasource';
+import { PermissoesTelasService } from './services/administracao/permissoesTelas.service';
+import {PermissoesTelasDataSouce} from './services/administracao/permissoesTelas.datasource';
 import {SearchService} from './services/administracao/search.service';
+import { ListaPermissoesSevices } from './services/administracao/listaPermissoes.service';
 
 @NgModule({
   providers: [
@@ -16,6 +19,9 @@ import {SearchService} from './services/administracao/search.service';
     AuthGuard,
     GrupoService,
     GruposDataSouce,
+    PermissoesTelasService,
+    PermissoesTelasDataSouce,
+    ListaPermissoesSevices,
     SearchService,
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
   ]
