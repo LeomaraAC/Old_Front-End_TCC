ee2f8f081ec01d9d5828709e1b71e1b415efe584
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-07-11 16:18:43 -0300

    Adicionando o observable na classe NotificationService que será responsável pelo snackbar.

--- src/app/core/services/notification.service.ts
+++ src/app/core/services/notification.service.ts
@@ -2,7 +2,11 @@ import {EventEmitter} from '@angular/core';
 
 export class NotificationService {
   notifier = new EventEmitter<string>();
+  notifierSnackbar = new EventEmitter<string[]>();
   notify(message: string) {
     this.notifier.emit(message);
+  }
+  notifySnackbar(message: string, erro: boolean) {
+    this.notifierSnackbar.emit([message, erro.toString()]);
   }
 }
